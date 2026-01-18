415db8f5b510f9a2eba8cb837cd77a81be289352
ppour-ba <pouriya.pourbahrami@gmail.com>
2026-01-18 22:39:30 +0100

    refactor(artifact): rename hitPoint to hitPoints for consistency
    - Rename hitPointBonus to hitPointsBonus
    - Update getter to match plural naming
    - Align terminology with hero hit points usage

--- src/main/java/com/pourist/swingy/model/artifact/Helm.java
+++ src/main/java/com/pourist/swingy/model/artifact/Helm.java
@@ -4,16 +4,16 @@ public class Helm extends Artifact {
 
     private final int hitPointBonus;
 
-    public Helm(String name, int hitPointBonus) {
+    public Helm(String name, int hitPointsBonus) {
         super(name);
 
-        if (hitPointBonus <= 0)
+        if (hitPointsBonus <= 0)
             throw new IllegalArgumentException("Hit point bonus must be positive");
 
-        this.hitPointBonus = hitPointBonus;
+        this.hitPointBonus = hitPointsBonus;
     }
 
-    public int getHitPointBonus() {
+    public int getHitPointsBonus() {
         return hitPointBonus;
     }
 }
