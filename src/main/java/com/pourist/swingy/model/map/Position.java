7be8fb2d014722ad6aad2ec35bf5a1566616f44b
ppour-ba <pouriya.pourbahrami@gmail.com>
2026-01-24 22:59:56 +0100

    feat(map): add Position value object for map coordinates

--- src/main/java/com/pourist/swingy/model/map/Position.java
+++ src/main/java/com/pourist/swingy/model/map/Position.java
@@ -1,4 +1,19 @@
 package com.pourist.swingy.model.map;
 
 public class Position {
+    final private int x;
+    final private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
 }
