d9cc2423e4d046ede7c749ef9944c8af6e09595d
pourist <pouriya.pourbahrami@gmail.com>
2026-01-28 02:04:22 +0100

    feat(main): add application entry point and view selection

--- src/main/java/com/pourist/swingy/Main.java
+++ src/main/java/com/pourist/swingy/Main.java
@@ -1,7 +1,29 @@
 package com.pourist.swingy;
 
+import com.pourist.swingy.controller.GameController;
+import com.pourist.swingy.view.ConsoleView;
+import com.pourist.swingy.view.GuiView;
+import com.pourist.swingy.view.View;
+
 public class Main {
     public static void main(String[] args) {
+        try {
 
+            View view;
+
+            if (args.length == 0 || args[0].equalsIgnoreCase("console")) {
+                view = new ConsoleView();
+            } else if (args[0].equalsIgnoreCase("gui")) {
+                view = new GuiView();
+            } else {
+                throw new IllegalArgumentException("Usage: console | gui");
+            }
+
+            GameController controller = new GameController(view);
+
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
