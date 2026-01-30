6387e3b6fadfbb8511c0db971079373d32643ad5
pourist <pouriya.pourbahrami@gmail.com>
2026-01-30 23:46:20 +0100

    feat(persistence): add DatabaseManager for SQLite initialization and schema setup

--- src/main/java/com/pourist/swingy/persistence/DatabaseManager.java
+++ src/main/java/com/pourist/swingy/persistence/DatabaseManager.java
@@ -1,4 +1,47 @@
 package com.pourist.swingy.persistence;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseManager {
+    private static final String DB_URL = "jdbc:sqlite:data/heroes.db";
+    private static Connection connection;
+
+    private DatabaseManager() {}
+
+    public static void init() {
+        try {
+            connection = DriverManager.getConnection(DB_URL);
+            createTableIfNotExist();
+        } catch (SQLException e) {
+            throw new IllegalStateException("Failed to initialize database", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null )
+            throw new IllegalStateException("Database not initialized");
+        return connection;
+    }
+
+    private static void createTableIfNotExist() throws SQLException {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS heroes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL UNIQUE,
+                    hero_class TEXT NOT NULL,
+                    level INTEGER NOT NULL,
+                    experience INTEGER NOT NULL,
+                    weapon TEXT,
+                    armor TEXT,
+                    helm TEXT
+                );
+                """;
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(sql);
+        }
+    }
 }
