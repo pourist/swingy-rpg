af367dff6d9bed866ce46cb4194f153a9fbe396e
pourist <pouriya.pourbahrami@gmail.com>
2026-01-31 01:59:58 +0100

    feat(persistence): add DatabaseManager for SQLite initialization and schema setup

--- src/main/java/com/pourist/swingy/persistence/DatabaseManager.java
+++ src/main/java/com/pourist/swingy/persistence/DatabaseManager.java
@@ -1,45 +1,55 @@
 package com.pourist.swingy.persistence;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseManager {
+public final class DatabaseManager {
+
     private static final String DB_URL = "jdbc:sqlite:data/heroes.db";
     private static Connection connection;
 
-    private DatabaseManager() {}
+    private DatabaseManager() {
+
+    }
 
     public static void init() {
         try {
             connection = DriverManager.getConnection(DB_URL);
-            createTableIfNotExist();
+            createTablesIfNotExist();
         } catch (SQLException e) {
             throw new IllegalStateException("Failed to initialize database", e);
         }
     }
 
     public static Connection getConnection() {
-        if (connection == null )
+        if (connection == null) {
             throw new IllegalStateException("Database not initialized");
+        }
         return connection;
     }
 
-    private static void createTableIfNotExist() throws SQLException {
+    private static void createTablesIfNotExist() throws SQLException {
         String sql = """
-                CREATE TABLE IF NOT EXISTS heroes (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL UNIQUE,
-                    hero_class TEXT NOT NULL,
-                    level INTEGER NOT NULL,
-                    experience INTEGER NOT NULL,
-                    weapon TEXT,
-                    armor TEXT,
-                    helm TEXT
-                );
-                """;
+            CREATE TABLE IF NOT EXISTS heroes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE,
+                hero_class TEXT NOT NULL,
+                level INTEGER NOT NULL,
+                experience INTEGER NOT NULL,
+
+                weapon_name  TEXT,
+                weapon_bonus INTEGER,
+
+                armor_name   TEXT,
+                armor_bonus  INTEGER,
+
+                helm_name    TEXT,
+                helm_bonus   INTEGER
+            );
+        """;
+
         try (Statement stmt = connection.createStatement()) {
             stmt.execute(sql);
         }
