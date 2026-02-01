f9ced6bf4a972ec02dd18c559a5c9c5a225cacb0
pourist <pouriya.pourbahrami@gmail.com>
2026-02-01 23:54:33 +0100

    refactor(model): replace Position class with immutable record

--- src/main/java/com/pourist/swingy/model/map/Position.java
+++ src/main/java/com/pourist/swingy/model/map/Position.java
@@ -1,31 +1,4 @@
 package com.pourist.swingy.model.map;
 
-public class Position {
-    final private int x;
-    final private int y;
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY(){
-        return y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Position)) return false;
-        Position p = (Position) o;
-        return x == p.x && y == p.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return 31 * x + y;
-    }}
+public record Position(int x, int y) {
+}
